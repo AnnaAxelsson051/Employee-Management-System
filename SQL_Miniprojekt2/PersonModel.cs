9be3d9c6fddd0a3a9437406f39b147daec62a515
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-22 16:48:24 +0100

    Add person model class

---
+++ SQL_Miniprojekt2/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SQL_Miniprojekt2
+{
+    public class PersonModel
+    {
+        public PersonModel()
+        {
+        }
+
+        public string personName { get; set; }
+    }
+}
+
