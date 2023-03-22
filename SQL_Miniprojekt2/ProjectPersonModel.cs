0e5f564b619f1a3855add16088a0a850e88f8136
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-22 16:49:28 +0100

    Add projectPerson model class to connect person to project

---
+++ SQL_Miniprojekt2/ProjectPersonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SQL_Miniprojekt2
+{
+    public class ProjectPersonModel
+    {
+        public ProjectPersonModel()
+        {
+        }
+
+        public int projectId { get; set; }
+
+        public string projectName { get; set; }
+
+        public int personId { get; set; }
+
+        public string personName { get; set; }
+
+        public int hours { get; set; }
+    }
+}
+
