240ad80f72595a423f9f266c317d9ccd090708da
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-22 16:48:45 +0100

    Add project model class

---
+++ SQL_Miniprojekt2/ProjectModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SQL_Miniprojekt2
+{
+    public class ProjectModel
+    {
+        public ProjectModel()
+        {
+        }
+
+        public string projectName { get; set; }
+    }
+}
+
